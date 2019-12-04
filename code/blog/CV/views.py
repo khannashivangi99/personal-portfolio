208aeda696479285b6864fa27fe8f7b45dfa5bb9
Arpit Khanna <arpit.k@Arpitkhanna.local>
2019-12-04 17:56:01 +0530

    changed cv path

--- code/blog/CV/views.py
+++ code/blog/CV/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 # Create your views here.
 class DownloadCV(APIView):
     def get(self, request):
-        static_file_path = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        static_file_path = settings.STATIC_ROOT
         pdf_path = f'{static_file_path}/assets/CV(Arpit Khanna).pdf'
         pdf_data = open(pdf_path, 'rb').read()
         return Response(pdf_data, mimetype='application/pdf')
