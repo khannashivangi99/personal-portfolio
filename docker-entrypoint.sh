d1e874050fb53ead270606f66d8402ccf1689e15
root <root@LAPTOP-FGL523VP.localdomain>
2020-09-29 20:36:25 +0530

    deleted useless files

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3.6 manage.py collectstatic --clear --noinput # clearstatic files
-python3.6 manage.py collectstatic --noinput  # collect static files
-# Prepare log files and start outputting logs to stdout
-echo Starting nginx 
-# Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn --bind 0.0.0.0:80 blog.wsgi --workers 3
-exec service nginx start
